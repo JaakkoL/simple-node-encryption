588f5e3e50f1d46dee089a4178f8ab0b2337b73b
JaakkoL <jaakko.o.laurila@gmail.com>
2016-01-08 16:44:21 +0100

    Improved tests and added test for different AES algorithms.

--- test/simple-node-encryption.js
+++ test/simple-node-encryption.js
@@ -4,22 +4,55 @@ var chai = require('chai')
 var should = chai.should()
 var encryption = require('../simple-node-encryption')
 
-var encryptionKey = '12345678901234567890123456789012' // For AES-256
 var secretString = 'This is a very secret message.'
 
+// openssl list-cipher-algorithms
+var algorithms = [
+  { name: 'aes-128-ecb', iv: getBuffer(), key: getKey(16) },
+  { name: 'aes-128-cbc', iv: getBuffer(16), key: getKey(16) },
+  { name: 'aes-192-ecb', iv: getBuffer(), key: getKey(24) },
+  { name: 'aes-192-cbc', iv: getBuffer(16), key: getKey(24) },
+  { name: 'aes-256-ecb', iv: getBuffer(), key: getKey(32) },
+  { name: 'aes-256-cbc', iv: getBuffer(16), key: getKey(32) }
+]
+
 describe('Simple data encryption', function() {
-  it('Should encrypt correctly Strings', function() {
-    var encrypted = encryption.encrypt(secretString, encryptionKey)
-    var decrypted = encryption.decrypt(encrypted, encryptionKey).toString('utf8')
 
-    decrypted.should.equal(secretString)
-  })
+  algorithms.forEach(function(algorithm) {
+    var encryptionService = encryption(algorithm.name, algorithm.iv)
 
-  it('Should encrypt correctly Buffers', function() {
-    var buffer = new Buffer(secretString, 'utf8')
-    var encrypted = encryption.encrypt(buffer, encryptionKey)
-    var decrypted = encryption.decrypt(encrypted, encryptionKey).toString('utf8')
+    describe('Testing ' + algorithm.name, function() {
+      it('Should encrypt and decrypt correctly Strings', function() {
+        testStringEncryption(encryptionService, algorithm.key)
+      })
 
-    decrypted.should.equal(secretString)
+      it('Should encrypt and decrypt correctly Buffers', function() {
+        testBufferEncryption(encryptionService, algorithm.key)
+      })
+    })
+
   })
 })
+
+function getKey(length) {
+  return 'abcdefghijklmnopqrstuvwxzyabcdefghijklmnopqrstuvwxzy'.slice(0, length)
+}
+
+function getBuffer(size) {
+  return (size) ? new Buffer(size).fill(0) : new Buffer('')
+}
+
+function testStringEncryption(encryptionService, encryptionKey) {
+  var encrypted = encryptionService.encrypt(secretString, encryptionKey)
+  var decrypted = encryptionService.decrypt(encrypted, encryptionKey).toString('utf8')
+
+  decrypted.should.equal(secretString)
+}
+
+function testBufferEncryption(encryptionService, encryptionKey) {
+  var buffer = new Buffer(secretString, 'utf8')
+  var encrypted = encryptionService.encrypt(buffer, encryptionKey)
+  var decrypted = encryptionService.decrypt(encrypted, encryptionKey).toString('utf8')
+
+  decrypted.should.equal(secretString)
+}
