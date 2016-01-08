08ce91b11a2857b96408e9347ffd8eb3d965ab75
JaakkoL <jaakko.o.laurila@gmail.com>
2016-01-08 16:43:46 +0100

    Make encryption service more generic.

--- simple-node-encryption.js
+++ simple-node-encryption.js
@@ -1,22 +1,29 @@
 'use strict'
 
 var crypto = require('crypto')
-var algorithm = 'aes-256-ecb'
-var iv = new Buffer('') // Buffer is really not needed with ECB.
 
-var exports = module.exports = {}
+function Encryption(algorithm, iv) {
+  iv = iv || new Buffer('')
 
-exports.encrypt = function(message, key) {
-  var buffer = (message instanceof Buffer) ? message : new Buffer(message, 'utf8')
-  var cipher = crypto.createCipheriv(algorithm, key, iv)
-  var crypted = Buffer.concat([cipher.update(buffer), cipher.final()])
+  function encrypt(message, key) {
+    var buffer = (message instanceof Buffer) ? message : new Buffer(message, 'utf8')
+    var cipher = crypto.createCipheriv(algorithm, key, iv)
+    var crypted = Buffer.concat([cipher.update(buffer), cipher.final()])
 
-  return crypted
+    return crypted
+  }
+
+  function decrypt(buffer, key) {
+    var decipher = crypto.createDecipheriv(algorithm, key, iv)
+    var decrypted = Buffer.concat([decipher.update(buffer), decipher.final()])
+
+    return decrypted
+  }
+
+  return {
+    encrypt: encrypt,
+    decrypt: decrypt
+  }
 }
 
-exports.decrypt = function (buffer, key) {
-  var decipher = crypto.createDecipheriv(algorithm, key, iv)
-  var decrypted = Buffer.concat([decipher.update(buffer), decipher.final()])
-
-  return decrypted
-}
+module.exports = Encryption
